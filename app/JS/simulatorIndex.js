d09d7ca7958022d98857cdae86c14500b1839254
David Rodriguez <alejandro20rodriguez04@gmail.com>
2020-06-03 15:32:37 -0700

    simulator buttons and table functionality added

--- app/JS/simulatorIndex.js
+++ app/JS/simulatorIndex.js
@@ -4,8 +4,22 @@ function init() {
     //show nav and header
     initMenu();
 
-    //creating an instance of callsimulador
+    // //creating an instance of callsimulador
     const Simulator = new CallSimulator();
-    //run simulator
-    Simulator.run();
+    //set table blody id
+    Simulator.setTableId("body");
+
+    //Get buttons
+    const btnStart = document.getElementById("start-simulation");
+    const btnStop = document.getElementById("stop-simulation");
+
+    //add listeners
+    btnStart.addEventListener("click", () => {
+        //run simulator
+        let activated = Simulator.run();
+    });
+    btnStop.addEventListener("click", () => {
+        //run simulator
+        let stopped = Simulator.stop();
+    });
 }
--- app/JS/utils/simulator.js
+++ app/JS/utils/simulator.js
@@ -128,10 +128,20 @@ class CallSimulator {
     }
 
     run() {
-        this.setCallTimeout();
+        console.log("Running simulator...");
+        if (this.setCallTimeout()) {
+            return true;
+        } else {
+            return false;
+        }
     }
     stop() {
-        clearTimeout(this.timeout);
+        console.log("Simulator was stopped.");
+        if (clearTimeout(this.timeout)) {
+            return true;
+        } else {
+            return false;
+        }
     }
     getTimeInterval() {
         //return a random number between min and max
@@ -161,7 +171,13 @@ class CallSimulator {
         //     }
         // );
         // const answer = await request.json();
-        // console.log(answer)
+        // if (answer.status == 0) {
+        //     //call added succesfully
+        this.insertToTable();
+        // console.log(answer);
+        // } else {
+        //     throw new Error(answer.message);
+        // }
     }
     generateCellNumber() {
         const number = Math.floor(Math.random() * 10000000);
@@ -188,11 +204,42 @@ class CallSimulator {
         }, 5000);
     }
     showInformation() {
-        const d = new Date();
         return {
             cellphone: this.cellphone,
-            currentTime: `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`,
+            currentTime: this.getCurrentTime(),
             nextCallIn: `${this.timeInterval / 1000} seconds`,
         };
+    }
+    getCurrentTime() {
+        const d = new Date();
+        const getSeconds = () => {
+            if (d.getSeconds() < 10) return `0` + d.getSeconds();
+            else return d.getSeconds();
+        };
+        const getMinutes = () => {
+            if (d.getMinutes() < 10) return `0` + d.getMinutes();
+            else return d.getMinutes();
+        };
+        const getHours = () => {
+            if (d.getHours() < 10) return `0` + d.getHours();
+            else return d.getHours();
+        };
+
+        return `${getHours()}:${getMinutes()}:${getSeconds()}`;
+    }
+    setTableId(id) {
+        id ? (this.tableId = id) : console.error("Table Body id missing");
+    }
+
+    insertToTable() {
+        const table = document.getElementById(this.tableId);
+        table.insertAdjacentHTML(
+            "afterbegin",
+            `<tr>
+                <td>${this.cellphone}</td>
+                <td>${this.getCurrentTime()}</td>
+                <td>${this.timeInterval / 1000} seconds</td>
+            </tr>`
+        );
     }
 }
